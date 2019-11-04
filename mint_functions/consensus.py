53d90ddd4a2333daefe71cb2a928540d0a537a42
pdawgsledge <34050364+pdawgsledge@users.noreply.github.com>
2019-11-03 18:37:42 -0600

    Consensus Form

--- mint_functions/consensus.py
+++ mint_functions/consensus.py
@@ -7,8 +7,20 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import time
 
+# Set scope of Google Sheets API (?)
+scope = ['https://spreadsheets.google.com/feeds',
+        'https://www.googleapis.com/auth/drive']
+
 credentials = ServiceAccountCredentials.from_json_keyfile_name('crypto-201803-ea9734b5c249.json', scope)
 gs = gspread.authorize(credentials)
 ss = gs.open("Consensus Form (Responses)")
 sheet = ss.worksheet("Responses")
+
+email = sheet.col_values(1)[1:]
+peer_email = sheet.col_values(2)[1:]
+date = sheet.col_values(3)[1:]
+execution = sheet.col_values(4)[1:]
+creativity = sheet.col_values(5)[1:]
+practicality = sheet.col_values(6)[1:]
+respect = sheet.col_values(7)[1:]
 
