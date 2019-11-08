94807af93dd572f5cd8b758e081b8f0b3945a92c
pdawgsledge <34050364+pdawgsledge@users.noreply.github.com>
2019-11-08 03:41:57 -0600

    Added mint functionality
    Need to add peer effects

--- mint_functions/consensus.py
+++ mint_functions/consensus.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from web3 import Web3, HTTPProvider
 import csv
 import tweepy
@@ -6,6 +7,7 @@ from datetime import datetime
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import time
+import numpy as np
 
 # Set scope of Google Sheets API (?)
 scope = ['https://spreadsheets.google.com/feeds',
@@ -13,15 +15,57 @@ scope = ['https://spreadsheets.google.com/feeds',
 
 credentials = ServiceAccountCredentials.from_json_keyfile_name('crypto-201803-ea9734b5c249.json', scope)
 gs = gspread.authorize(credentials)
-ss = gs.open("Consensus Form (Responses)")
-sheet = ss.worksheet("Responses")
+ss = gs.open("Master Sheet")
+sheet = ss.worksheet("Consensus")
 
-email = sheet.col_values(1)[1:]
-peer_email = sheet.col_values(2)[1:]
-date = sheet.col_values(3)[1:]
-execution = sheet.col_values(4)[1:]
-creativity = sheet.col_values(5)[1:]
-practicality = sheet.col_values(6)[1:]
-respect = sheet.col_values(7)[1:]
+responses = sheet.get_all_values()
+headers = responses.pop(0)
+
+df = pd.DataFrame(responses, columns=headers)
+
+w3 = Web3(HTTPProvider("https://ropsten.infura.io/v3/4e529bfe5adb43d49db599afcf381cd3"))
+
+# Get token contract address and creator's keys
+contract_address = w3.toChecksumAddress('0x35cae81Ed8Ed242E7db6eDcafEAb04A91cD60184')
+public_key = w3.toChecksumAddress('0xCbcFfBecdB81698DDF3504d4E7dbeD8565f02715')
+private_key = 'CDF28FC7FDCDA6126BE2ECE17CD2008F7C4FB77F25E07F5D810F933BA72E0FA2'
+
+# Get contract object using web3
+contract = w3.eth.contract(address=contract_address, abi=contract_abi.abi)
+
+# Loop through list of addresses (passed as an argument)
+for pk, peer in zip(np.unique(df['Ethereum Address']), df['Peer - "Firstname Lastname"']):
+
+    sheet = ss.worksheet("Participants")
+
+    responses = sheet.get_all_values()
+    headers = responses.pop(0)
+
+    df = pd.DataFrame(responses, columns=headers)
+
+    pks = sheet.col_values(1)
+    peer_pk = [i[0] for i in zip(df['Address'], df['Name']) if i[1] == peer]
+
+    # Get transaction count of minter address to use as nonce for next transaction
+    tx_count = w3.eth.getTransactionCount(public_key)
+
+    # Build transaction
+    mint_tx = contract.functions.burn(pk, 2000000000000000000)
+    print(mint_tx)
+
+    tx = mint_tx.buildTransaction({'gas': 1000000, 'nonce': tx_count})
+    print(tx)
+
+    # Sign transaction
+    signed = w3.eth.account.signTransaction(tx, private_key)
 
 
+    # Send transaction
+    txn_hash = w3.eth.sendRawTransaction(signed.rawTransaction)
+    success = w3.eth.waitForTransactionReceipt(txn_hash)
+    print(success)
+
+    # Send transaction
+    txn_hash = w3.eth.sendRawTransaction(signed.rawTransaction)
+    success = w3.eth.waitForTransactionReceipt(txn_hash)
+    print(success)
