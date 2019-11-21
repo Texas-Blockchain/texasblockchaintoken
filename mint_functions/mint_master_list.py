df8368af597775b0c318c950ef4ba64043a30071
Jack Sharkey <sharkeyjack11@gmail.com>
2019-11-20 18:44:12 -0600

    Mint master is dynamic

--- mint_functions/mint_master_list.py
+++ mint_functions/mint_master_list.py
@@ -31,7 +31,7 @@ def list_of_tweets():
     credentials = ServiceAccountCredentials.from_json_keyfile_name('crypto-201803-ea9734b5c249.json', scope)
     gs = gspread.authorize(credentials)
     ss = gs.open("Master Spreadsheet")
-    sheet = ss.worksheet("Tweets")
+    sheet = ss.worksheet("Payouts")
     return sheet.get_all_values()
 
  #Go through all tweets in sheet and if we have their tbt address and they haven't been paid for that tweet, mint           
@@ -45,25 +45,36 @@ def payout_retweeters():
     for tweet in tweets:
         handle = tweet[1]
         paid_out = tweet[2]
+        amount = tweet[3]
+        tbt_address = tweet[4]
+
         if handle in participants.keys() and paid_out == 'FALSE':
             address = participants[handle]
-            payout_list.append([address, cell_number])
+            payout_list.append([address, cell_number, amount])
+        elif len(tbt_address) == 42 and paid_out == 'FALSE':
+            payout_list.append([tbt_address, cell_number, amount])
         cell_number += 1
-    mint_tbt(payout_list)
+    if len(payout_list) > 0:
+        mint_tbt(payout_list)
 
 #Mint $TBT to a list of addresses
 #Recieves a list of lists containing [tbt address,row number]
 #After minting, update the cell address to TRUE
+real_contract = '0x9e2b0752131CB2F424f39Ce509CccB230Cd9304f'
+real_public_key = '0x6D10875b3C3F53F1C1cf9aE130B1A415790f8Cf3'
+real_private_key = '74BC2E012071FCA65CA31FA7AFE0483929A451180ACBF9D9647A06AC30E69957'
 def mint_tbt(addresses):
+    print("Paying out to:")
+    print(str(addresses))
     # Connect to an Ethereum node using Infura
     w3 = Web3(HTTPProvider("https://ropsten.infura.io/v3/4e529bfe5adb43d49db599afcf381cd3"))
 
     # Get contract address and owner's keys
-    contract_address = w3.toChecksumAddress('0x35cae81ed8ed242e7db6edcafeab04a91cd60184')
-    public_key = w3.toChecksumAddress('0xCbcFfBecdB81698DDF3504d4E7dbeD8565f02715')
-    private_key = 'CDF28FC7FDCDA6126BE2ECE17CD2008F7C4FB77F25E07F5D810F933BA72E0FA2'
+    contract_address = w3.toChecksumAddress('0x9e2b0752131CB2F424f39Ce509CccB230Cd9304f')
+    public_key = w3.toChecksumAddress('0x6D10875b3C3F53F1C1cf9aE130B1A415790f8Cf3')
+    private_key = '74BC2E012071FCA65CA31FA7AFE0483929A451180ACBF9D9647A06AC30E69957'
     #TODO: CORRECT DIR NAME
-    dirname = "/home/bitcoinnode/texasblockchaintoken/mint_functions"
+    dirname = "/Users/jacksharkey/Documents/texasblockchain/tbt/mint_functions"
 
     # Get contract object using web3
     contract = w3.eth.contract(address=contract_address, abi=contract_abi.abi)
@@ -74,23 +85,27 @@ def mint_tbt(addresses):
     for address in addresses:
         tbt_address = address[0]
         cell = address[1]
+        amount = address[2]
+        tbt_address = w3.toChecksumAddress(tbt_address)
         # Get transaction count of address to use as nonce for next transaction
         tx_count = w3.eth.getTransactionCount(public_key)
-
+        wei_amount = w3.toWei(amount, 'ether')
         # Build transaction
-        mint_tx = contract.functions.mint(tbt_address, 10000000000000000000).buildTransaction({'gas': 1000000, 'nonce': tx_count})
-
+        mint_tx = contract.functions.mint(tbt_address, wei_amount).buildTransaction({'gas': 1000000, 'nonce': tx_count})
         # Sign transaction
         signed = w3.eth.account.signTransaction(mint_tx, private_key)
 
         # Submit transaction
         txn_hash = w3.eth.sendRawTransaction(signed.rawTransaction)
+        success = w3.eth.waitForTransactionReceipt(txn_hash)
         txn_hashes.append(txn_hash.hex())
+        print("Mint successful.")
 
         #Mark as paid on sheet
         update_sheet(cell)
+        # time.sleep(3)
 
-    print(txn_hashes)
+    # print(txn_hashes)
 
 
 #Mark tweet as paid
@@ -98,7 +113,10 @@ def update_sheet(rowNumber):
     credentials = ServiceAccountCredentials.from_json_keyfile_name('crypto-201803-ea9734b5c249.json', scope)
     gs = gspread.authorize(credentials)
     ss = gs.open("Master Spreadsheet")
-    sheet = ss.worksheet("Tweets")
+    sheet = ss.worksheet("Payouts")
     return sheet.update_cell(rowNumber,3,'TRUE')
 
-# payout_retweeters()
+while(1):
+    print("Checking")
+    payout_retweeters()
+    time.sleep(60)
